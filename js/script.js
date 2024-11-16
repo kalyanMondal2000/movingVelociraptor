59bc19e05aec87c3bd3697d2185b630b04583a69
kalyanMondal2000 <kalyan.x.mondal@gmail.com>
2024-11-16 16:30:17 -0500

    impored skeletonUtils

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-import * as SkeletonUtils from './js/SkeletonUtils.js';
+import SkeletonUtils from 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/utils/SkeletonUtils.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
