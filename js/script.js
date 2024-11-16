e8a2ad273289236fdf1215150d84528c2a83ddc3
kalyanMondal2000 <kalyan.x.mondal@gmail.com>
2024-11-16 13:59:50 -0500

    added animations for velociraptor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
+import * as SkeletonUtils from './Users/xaviermondal/Desktop/three/threejs/resources/threejs/r122/examples/js/utils/SkeletonUtils.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -23,6 +25,28 @@ loader.load(
   }
 );
 
+function prepModelsAndAnimations() {
+  const animsByName = {};
+  './models/velociraptor.glb'.gltf.animations.forEach((clip)=>{
+    animsByName[clip.name] = clip;
+  })
+  './models/velociraptor.glb'.animations = animsByName;
+}
+
+const mixers = [];
+
+prepModelsAndAnimations();
+const clonedScene = SkeletonUtils.clone('./models/velociraptor.glb'.glb.scene);
+const root = new THREE.Object3D();
+root.add(clonedScene);
+scene.add(root);
+root.position.x = (ndx - 3) * 3;
+
+const mixer = new THREE.AnimationMixer(clonedScene);
+const firstClip = Object.values('./models/velociraptor.glb'.animations)[0];
+const action = mixer.clipAction(firstClip);
+action.play();
+mixers.push(mixer);
 
 const renderer = new THREE.WebGLRenderer({ alpha: true }); 
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -35,15 +59,39 @@ topLight.position.set(500, 500, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
+let then = 0;
+
+function render(now) {
+  now *= 0.001;  // convert to seconds
+  const deltaTime = now - then;
+  then = now;
+ 
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+ 
+  for (const mixer of mixers) {
+    mixer.update(deltaTime);
+  }
+ 
+  renderer.render(scene, camera);
+ 
+  requestAnimationFrame(render);
+}
+
+
+/*
 const moveSpeed = 0.5; 
 const acceleration = 0.5; 
 let moveDirection = new THREE.Vector3(0, 0, 0);
 const targetDirection = new THREE.Vector3(0, 0, 0);
-
+*/
 const ambientLight = new THREE.AmbientLight(0x333333, 10);
 scene.add(ambientLight);
 
-
+/*
 document.addEventListener('keydown', (event) => {
   
   if (event.key === 'w') targetDirection.y = 1;
@@ -79,7 +127,7 @@ function animate() {
 
   renderer.render(scene, camera);
 }
-
+*/
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -88,4 +136,4 @@ window.addEventListener("resize", function () {
 
 
 
-animate();
+//animate();
